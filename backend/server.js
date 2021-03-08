1e9cf331374e20c67f11204e3277a8db4d375cd3
Shreyash Patel <patelshreyash412@gmail.com>
2021-03-08 20:39:24 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express  from 'express'
+import path from 'path'
 import cors from 'cors'
 import dotenv  from 'dotenv'
 import connectDB from './config/db.js'
@@ -16,12 +17,21 @@ app.use(express.json())
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+
 
 app.use('/api/tasks',taskRoutes)
 app.use('/api/users',userRoutes)
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
 
 app.use(notFound)
 
